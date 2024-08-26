d0533a5bb201707bc96eaef3268fd2fbcc98e6ff
sarawbagheri <sarawbagheri@gmail.com>
2024-08-26 18:09:55 +0330

    adding tests for agreementAPI

--- tests/GetLoan.api.test.js
+++ tests/GetLoan.api.test.js
@@ -1,69 +1,107 @@
-require('dotenv').config();
-const axios = require('axios');
+require("dotenv").config();
+const axios = require("axios");
 
-const baseUrl=process.env.WALLPAY_TEST_BASEURL;
-const bearerToken=process.env.WALLPAY_BEARER_TOKEN;
+const baseUrl = process.env.WALLPAY_TEST_BASEURL;
+const bearerToken = process.env.WALLPAY_BEARER_TOKEN;
 
-describe ('GetLoan API tests', () => {
+describe("GetLoan API tests", () => {
+  describe("checking agreement of every returned package", () => {
+    test("checking correctness of agreement response for available packages (none CnBs)", async () => {
+      const responseOfPackages = await axios.get(
+        `${baseUrl}/v1/loans/packages`,
+        {
+          headers: {
+            Authorization: `Bearer ${bearerToken}`,
+          },
+        },
+      );
+      expect(responseOfPackages.status).toBe(200);
 
+      for (let item of responseOfPackages.data.data) {
+        if ((item.available == true) & (item.name != "CnB")) {
+          const agreement = await axios.post(
+            `${baseUrl}/v1/loans/agreement`,
+            { loanAmount: item.amount, loanConfigID: item.loanConfigID },
+            {
+              headers: {
+                Authorization: `Bearer ${bearerToken}`,
+              },
+            },
+          );
 
-    describe ('checking agreement of every returned package',()=>{
+          expect(agreement.status).toBe(201);
 
-        test ('checking agreement of every returned package', async()=>{
-       
-            const responseOfPackages= await axios.get(`${baseUrl}/v1/loans/packages?loanProvider=digikala`,{
-                headers:{
-                    Authorization: `Bearer ${bearerToken}`
-                }
-            });
-            expect(responseOfPackages.status).toBe(200);
-            // console.log(responseOfPackages.data);
-    
-            for (let item of responseOfPackages.data.data){
+          const packageConfigId = item.loanConfigID;
+          const packageCollateralPrecision = item.asset.precision;
+          const packageAssetAmount = item.assetAmount;
 
-                const agreement = await axios.post(`${baseUrl}/v1/loans/agreement`,
-                    {loanAmount: item.amount , loanConfigID: item.loanConfigID},
-                    {
-                        headers:{
-                            Authorization: `Bearer ${bearerToken}`
-                        }}
-                );
+          const collateralPrice =
+            agreement.data.loanConfig.collateralAsset.value;
+          const collateralPrecision =
+            agreement.data.loanConfig.collateralAsset.precision;
+          const ltv = agreement.data.loanConfig.ltv;
+          const liquidationLtv = agreement.data.loanConfig.liquidationLtv;
+          const warningLtv = agreement.data.loanConfig.warningLtv;
+          const karma = agreement.data.loanConfig.karmaRate;
+          const ltvThreshold = "0.9";
+          const liquidationLtvThreshold = "0.95";
+          const warningLtvThreshold = "0.92";
+          const validLtv = agreement.data.loanConfig.validLtv;
+          const validLiquidationLtv =
+            agreement.data.loanConfig.validLiquidationLtv;
+          const validWarningLtv = agreement.data.loanConfig.validWarningLtv;
+          const repaymentAmount = agreement.data.repaymentAmount;
+          const loanAmount = agreement.data.loanAmount;
+          const collateralAmount = agreement.data.collateralAmount;
+          const liquidationAmount = agreement.data.liquidationAmount;
+          const minAmount = agreement.data.loanConfig.minAmount;
+          const maxAmount = agreement.data.loanConfig.maxAmount;
 
-                console.log(agreement.data);
-                expect(agreement.status).toBe(201);
+          toFixedPackageAssetAmount = Number(packageAssetAmount).toFixed(
+            packageCollateralPrecision,
+          );
+          expect(collateralAmount).toBe(toFixedPackageAssetAmount);
 
-                const collateralPrice=agreement.data.loanConfig.collateralAsset.value;
-                const collateralPrecision=agreement.data.loanConfig.collateralAsset.precision;
-                const ltv=agreement.data.loanConfig.ltv;
-                const karma=agreement.data.loanConfig.karmaRate;
-                const ltvThreshold= "0.9";
-                const validLtv=agreement.data.loanConfig.validLtv;
-                const repaymentAmount=agreement.data.repaymentAmount;
-                const loanAmount=agreement.data.loanAmount;
-                const collateralAmount=agreement.data.collateralAmount;
-            
-                
-                expect(loanAmount).toBe(item.amount);
-                expect(collateralAmount).toBe(((repaymentAmount/validLtv)/collateralPrice).toFixed(collateralPrecision));
-                
-                let newLtv=karma*ltv;
-                if (newLtv > ltvThreshold){
-                    newLtv= ltvThreshold;
-                }
-                newLtv= newLtv.toString();
-                expect(validLtv).toBe(newLtv);
-                
-                
-            }
-    
-    
-        });
-       
-        
+          expect(loanAmount).toBe(item.amount);
+          expect(collateralAmount).toBe(
+            (repaymentAmount / validLtv / collateralPrice).toFixed(
+              collateralPrecision,
+            ),
+          );
+
+          const loanAmountNum = Number(loanAmount);
+          const minAmountNum = Number(minAmount);
+          const maxAmountNum = Number(maxAmount);
+
+          expect(loanAmountNum).toBeGreaterThanOrEqual(minAmountNum);
+          expect(loanAmountNum).toBeLessThanOrEqual(maxAmountNum);
+
+          let newLtv = karma * ltv;
+          if (newLtv > ltvThreshold) {
+            newLtv = ltvThreshold;
+          }
+          newLtv = newLtv.toString();
+
+          let newLiquidationLtv = karma * liquidationLtv;
+          if (newLiquidationLtv > liquidationLtvThreshold) {
+            newLiquidationLtv = liquidationLtvThreshold;
+          }
+          newLiquidationLtv = newLiquidationLtv.toString();
+
+          let newWarningLtv = karma * warningLtv;
+          if (newWarningLtv > warningLtvThreshold) {
+            newWarningLtv = warningLtvThreshold;
+          }
+          newWarningLtv = newWarningLtv.toString();
+          expect(validLtv).toBe(newLtv);
+          expect(validLiquidationLtv).toBe(newLiquidationLtv);
+          expect(validWarningLtv).toBe(newWarningLtv);
+
+          expect(liquidationAmount).toBe(
+            (repaymentAmount / validLiquidationLtv).toFixed(0),
+          );
+        }
+      }
     });
-
- 
-
-
-
+  });
 });
